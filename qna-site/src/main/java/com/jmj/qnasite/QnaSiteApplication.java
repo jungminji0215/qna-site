1930f4e20061953de51e48a83285dd62ab496db4
jungminji <wjdalswl0215@gmail.com>
2022-01-16 04:11:16 +0900

    개발 환경 세팅

---
+++ qna-site/src/main/java/com/jmj/qnasite/QnaSiteApplication.java
@@ -0,0 +1,13 @@
+package com.jmj.qnasite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaSiteApplication.class, args);
+	}
+
+}
