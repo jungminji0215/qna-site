7a045b96690e8470a831c4bf7d0088f64de6e860
jungminji <wjdalswl0215@gmail.com>
2022-01-26 15:28:08 +0900

    [질문 조회][기능][#9] 게시글 데이터 조회

--- src/main/java/com/jmj/qnasite/controller/ArticleController.java
+++ src/main/java/com/jmj/qnasite/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.jmj.qnasite.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -32,5 +36,19 @@ public class ArticleController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("articles/{id}")
+    public String findById(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+
+        // 아이디로 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 가져온 데이터 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 보여줄 페이지 설정
+        return "articles/show";
     }
 }
